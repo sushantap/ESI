72f035cd4ea57a2f6be18ebefb15221740446916
sushanta pradhan <sushanta.pradhan@gmail.com>
2014-03-20 16:04:35 +0530

    removing junk charecters

--- WEB_SERVER/src/main/java/com/talentica/esi/test/ArticleFactory.java
+++ WEB_SERVER/src/main/java/com/talentica/esi/test/ArticleFactory.java
@@ -32,21 +32,20 @@ public class ArticleFactory {
 		Thread.sleep(1000);
 		return new Article("Comic book targets children in slums",
 				"There are no superheroes in this comic book. There is no "+ 
-						"âslumdog millionaireâ either. There are only messages for children "+
-						"living in slums; to stay away from tobacco and alcohol, to know âbad "+
-						"touchâ and to keep the environment clean, among others. âReach "+
-						"out! With Veera and Cheraâ â a comic book with seven episodes in "+
+						"slumdog millionaire either. There are only messages for children "+
+						"living in slums; to stay away from tobacco and alcohol, to know bad "+
+						"touch and to keep the environment clean, among others. Reach "+
+						"out! With Veera and Chera  a comic book with seven episodes in "+
 						"it, by Loyola College student Vijay Asokan, was released at the "+
-						"Outreach Dayâ2014 celebrations of the collegeâs department of "+
+						"Outreach Day2014 celebrations of the colleges department of "+
 						"outreach, on Monday. It is a simple strip in the form of an "+
 						"interaction between Veera, a Loyola college student who visits the "+
 						"slums as part of the outreach programme, and Chera, a child from the "+
 						"slum who handles challenges normally faced by children in slums in the "+
-						"cities. âChildren in slums do not know the serious problems in life. "+
+						"cities. Children in slums do not know the serious problems in life. "+
 						"That is why the comic strip has been designed. Apart from creating "+
 						"awareness on the harmful effects of tobacco and alcohol use, there is "+
-				"also an episode explaining child abuse,â says Vijay.");
-
+				"also an episode explaining child abuse, says Vijay.");
 	}
 
 	public static Article getArticle3() throws InterruptedException{
